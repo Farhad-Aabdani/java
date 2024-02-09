02fc082e1fe315459215c0145fa68e521f0893ff
Farhad Ali Aabdani <159477249+Farhad-Aabdani@users.noreply.github.com>
2024-02-09 11:35:54 +0500

    Update Bubblesorting.java

--- Bubblesorting.java
+++ Bubblesorting.java
@@ -3,7 +3,7 @@ package Java_2K23.DSA;
 public class Bubblesorting {
     public static void main(String[] args){
         int[]array = {7, 8, 3, 2, 1};
-        // buble sort
+        // bubble sort
         for(int i=0;i<array.length-1;i++){ // 4 time loop required to perform the sorting
             for(int j=0;j<array.length-i-1;j++){ // 5-0-1 = 4. 5-1-1=3.5-2-1=2. 5-3-1=1
                 if(array[j]>array[j+1]){ // 7>8 no; 8>3 yes
@@ -12,7 +12,7 @@ public class Bubblesorting {
                     array[j+1] = temp; // 3=8;
                 }
             }
-        }
+        } // printing the sorted array
         for(int i=0;i< array.length;i++){
             System.out.print(array[i]+" ");
         }
