eee159785b8688912188e60e22655f03545a58db
Farhad-Aabdani <159477249+Farhad-Aabdani@users.noreply.github.com>
2024-02-09 10:54:03 +0500

    Update FibnacciSeriesByRecurrsion.java
    Description: Generating Fibonacci Series Using Recursion
    The Java program FibonacciSeriesByRecursion illustrates the generation of the Fibonacci series through recursion. The Fibonacci series is a sequence of numbers where each number is the sum of the two preceding ones, usually starting with 0 and 1.
    
    Key Components:
    printFibonacci(int a, int b, int n) Method:
    
    This method prints the Fibonacci series starting from the first two terms a and b up to the nth term using recursion.
    It utilizes a base case to terminate recursion when n reaches 0.
    Inside the recursion, it calculates the next term c by adding the two preceding terms a and b, prints c, and calls itself recursively with updated values of b, c, and n-1.
    main(String[] args) Method:
    
    The main method initializes the first two terms a and b of the Fibonacci series (usually 0 and 1) and specifies the number of terms n to be printed.
    It then calls the printFibonacci method with the initialized values of a, b, and n-2 to print the Fibonacci series excluding the initial terms a and b.
    Usage:
    To run the program, compile the Java file and execute the main method.
    The program prints the Fibonacci series up to the specified number of terms, excluding the initial terms a and b.
    Example Output:
    For instance, setting n = 7 will output the Fibonacci series: 0 1 1 2 3 5 8.
    Customization:
    Users can modify the initial terms a and b and the number of terms n in the main method to generate different Fibonacci series as per their requirements.

--- FibnacciSeriesByRecurrsion.java
+++ FibnacciSeriesByRecurrsion.java
@@ -1,15 +1,15 @@
 package Java_2K23.DSA;
-
+// 0 1 1 2 3 5 8 Desired output.
 public class FibnacciSeriesByRecurrsion {
     public static void PrintFib(int a,int b,int n){
     // base case
         if(n==0){
             return ;
-        }// 0 1 1 2 3 5 8
-        int c=a+b;// c=>1
-        //c= b+c
+        } 
+        int c=a+b;// c= 0+1. c=>1
+        
         System.out.print(c+" ");
-        PrintFib(b,c,n-1);
+        PrintFib(b,c,n-1);// passing 'a' as b and 'b' as c. and minus n by 1 each time 
     }
     public static void main(String[] args){
         int a=0,b=1,n=7;
